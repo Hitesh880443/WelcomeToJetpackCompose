7a26a7d2eb462d760b992025a766402f0d5f2ed2
Hitesh880443 <hsutar00@gmail.com>
2020-10-27 22:07:55 +1100

    First commit

---
+++ app/src/main/java/com/hitesh/myjetpackcomposeapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.hitesh.myjetpackcomposeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.hitesh.myjetpackcomposeapp.ui.MyJetPackComposeAppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAppPage()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPage() {
+    MaterialTheme {
+        Text(text = "Hi Jetpack compose")
+    }
+}
